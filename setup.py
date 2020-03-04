77633dc29fa3f4172480a685d91b2a9d0cccd6d3
Steven Herbst <sherbst@stanford.edu>
2020-03-03 18:51:13 -0800

    try to fix pypi auth issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.0.5'
+version = '0.0.6'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
