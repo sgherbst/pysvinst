7305e24975a495cbc240cdf7a0ccf7151511332f
Steven Herbst <sherbst@stanford.edu>
2020-03-03 16:40:01 -0800

    try to fix auth for twine

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.0.2'
+version = '0.0.3'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
