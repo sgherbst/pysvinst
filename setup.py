a15de81d401b01b1a602a3e288a8176dc49b91c2
Steven Herbst <sherbst@stanford.edu>
2020-03-03 16:14:25 -0800

    fix wheel dep

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.0.1'
+version = '0.0.2'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
