8ed8b06e5d1dde4c01621f482fcd08c2c874efab
Steven Herbst <sherbst@stanford.edu>
2020-10-27 16:37:12 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.1.7.dev1'
+version = '0.1.7.dev18'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
