95f776071859316a6bdf414c7d9f095d1f8e17bc
Steven Herbst <sherbst@stanford.edu>
2020-10-28 12:17:15 -0700

    try using an older version of rust

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.1.7.dev26'
+version = '0.1.7.dev27'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
