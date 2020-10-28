d902121d5b077fed0aca0178984b48ec33b8113d
Steven Herbst <sherbst@stanford.edu>
2020-10-27 17:05:04 -0700

    updates to github actions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.1.7.dev21'
+version = '0.1.7.dev22'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
