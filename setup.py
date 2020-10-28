5ea982d9263bdf88146e666b1e4cb6c799093b68
Steven Herbst <sherbst@stanford.edu>
2020-10-28 12:10:30 -0700

    try switching docker image

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 name = 'svinst'
-version = '0.1.7.dev25'
+version = '0.1.7.dev26'
 
 DESCRIPTION = '''\
 Python library for parsing module definitions and instantiations from SystemVerilog files\
